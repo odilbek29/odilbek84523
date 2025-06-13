6e7cce3d3839c3a1a9657a6a72df1dcda2d69c76
odilbek29 <kamoliddinpardaev272@gmail.com>
2025-06-13 13:16:30 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import  './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Tojiqulov Odilbek</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident blanditiis repellendus atque? Necessitatibus, dolor nobis non cum laborum magnam facere magni ipsum molestiae est incidunt ea debitis neque molestias atque? Fuga quisquam, error accusamus non consequuntur debitis natus ullam reprehenderit.</p>
+      <ul>
+        <li>App.js</li>
+        <li>App.js</li>
+        <li>HTML</li>
+        <li>App.js</li>
+        <li>App.js</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
